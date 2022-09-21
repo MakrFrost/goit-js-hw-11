0fe0a878c057c0d185e0a5c1074b3f5f3b1c1605
MakrFrost <frostmark@ukr.net>
2022-09-21 17:30:18 +0300

    upd

--- src/js/fetchModule.js
+++ src/js/fetchModule.js
@@ -1,15 +1,55 @@
 const API_WEB = 'https://pixabay.com/api/';
 const API_KEY = '?key=30054612-8d338f00f07d687f9e2b96ac6&';
+const API_ORDER = 8;
 
-function fetchPhotos(needToFind, page) {
-  return fetch(
-    `${API_WEB}${API_KEY}q=${needToFind}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${page}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+export default class PhotoApiService {
+  constructor() {
+    this.needToFind = '';
+    this.page = 1;
+  }
+
+  fetchPhotos() {
+    return fetch(
+      `${API_WEB}${API_KEY}q=${this.needToFind}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${API_ORDER}&page=${this.page}`
+    )
+      .then(responce => responce.json())
+      .then(data => {
+        this.incrementPage();
+        console.log(data);
+        return data.hits;
+      });
+  }
+
+  addPhotos() {
+    this.page += 1;
+  }
+
+  resetPhotos() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.needToFind;
+  }
+
+  set query(newQuery) {
+    this.needToFind = newQuery;
+  }
 }
 
-export { fetchPhotos };
+//? код без класса(олд)
+// const API_WEB = 'https://pixabay.com/api/';
+// const API_KEY = '?key=30054612-8d338f00f07d687f9e2b96ac6&';
+
+// function fetchPhotos(needToFind, page) {
+//   return fetch(
+//     `${API_WEB}${API_KEY}q=${needToFind}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${page}`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// export { fetchPhotos };
--- src/js/resultModule.js
+++ src/js/resultModule.js
@@ -1,45 +1,32 @@
-import { fetchPhotos } from './fetchModule';
 import Notiflix from 'notiflix';
+import PhotoApiService from './fetchModule';
 const axios = require('axios');
 
-const inputEl = document.querySelector('[name="searchQuery"]');
-const buttonEl = document.querySelector('.search-button');
+const photoApiService = new PhotoApiService();
+
+//* форма поиска
+const formEl = document.querySelector('.search-form');
+//* галерея с фото
 const gallery = document.querySelector('.gallery');
-const loadMoreButton = document.querySelector('.load-more');
+//* кнопка "показать больше"
+const loadMoreBtn = document.querySelector('.load-more');
 
-buttonEl.addEventListener('click', onFormSubmit);
-//!
-loadMoreButton.addEventListener('click', onLoadMorePressed);
-
-let pages = 1;
+formEl.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
 //! функционал
 function onFormSubmit(event) {
   event.preventDefault();
-  let searchPhotos = inputEl.value.trim();
 
-  fetchPhotos(searchPhotos, pages).then(data => {
-    if (data.total > 1) {
-      gallery.innerHTML = '';
-      createPhotoEl(data.hits);
-      Notiflix.Notify.warning(`Hooray! We found ${data.totalHits} images.`);
-      Notiflix.Notify.success(`We find you photo!`);
-      // onLoadMorePressed();
-      createPhotoEl(data.hits);
-      loadMoreButton.style.cssText = 'visibility: visible';
-      pages = 1;
-    } else if (data.total === 0) {
-      gallery.innerHTML = '';
-      Notiflix.Notify.failure(
-        '"Sorry, there are no images matching your search query. Please try again."'
-      );
-    }
-
-    // console.log(data.hits)
-  });
+  photoApiService.query = event.currentTarget.elements.query.value;
+  photoApiService.resetPhotos();
+  photoApiService.fetchPhotos().then(createPhotoEl);
 }
 
-//? разметка для создания галереи
+function onLoadMore() {
+  photoApiService.fetchPhotos().then(createPhotoEl);
+}
+
 function createPhotoEl(photos) {
   const photoList = photos.map(photo => {
     return `<div class="photo-card">
@@ -65,9 +52,99 @@ function createPhotoEl(photos) {
   });
 }
 
-//!
-function onLoadMorePressed() {
-  pages += 1;
-  console.log(pages);
-  onFormSubmit(event);
-}
+//   fetchPhotos(searchPhotos, pages).then(data => {
+//     if (data.total > 1) {
+//       gallery.innerHTML = '';
+//       createPhotoEl(data.hits);
+//       Notiflix.Notify.warning(`Hooray! We found ${data.totalHits} images.`);
+//       Notiflix.Notify.success(`We find you photo!`);
+//       // onLoadMorePressed();
+//       createPhotoEl(data.hits);
+//       loadMoreBtn.style.cssText = 'visibility: visible';
+//       pages = 1;
+//     } else if (data.total === 0) {
+//       gallery.innerHTML = '';
+//       Notiflix.Notify.failure(
+//         '"Sorry, there are no images matching your search query. Please try again."'
+//       );
+//     }
+
+//     // console.log(data.hits)
+//   });
+// }
+
+//? код без класса(олд)
+// import { fetchPhotos } from './fetchModule';
+// import Notiflix from 'notiflix';
+
+// const axios = require('axios');
+
+// const inputEl = document.querySelector('[name="searchQuery"]');
+// const buttonEl = document.querySelector('.search-button');
+// const gallery = document.querySelector('.gallery');
+// const loadMoreButton = document.querySelector('.load-more');
+
+// buttonEl.addEventListener('click', onFormSubmit);
+// //!
+// loadMoreButton.addEventListener('click', onLoadMorePressed);
+
+// let pages = 1;
+
+// //! функционал
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   let searchPhotos = inputEl.value.trim();
+
+//   fetchPhotos(searchPhotos, pages).then(data => {
+//     if (data.total > 1) {
+//       gallery.innerHTML = '';
+//       createPhotoEl(data.hits);
+//       Notiflix.Notify.warning(`Hooray! We found ${data.totalHits} images.`);
+//       Notiflix.Notify.success(`We find you photo!`);
+//       // onLoadMorePressed();
+//       createPhotoEl(data.hits);
+//       loadMoreButton.style.cssText = 'visibility: visible';
+//       pages = 1;
+//     } else if (data.total === 0) {
+//       gallery.innerHTML = '';
+//       Notiflix.Notify.failure(
+//         '"Sorry, there are no images matching your search query. Please try again."'
+//       );
+//     }
+
+//     // console.log(data.hits)
+//   });
+// }
+
+// //? разметка для создания галереи
+// function createPhotoEl(photos) {
+//   const photoList = photos.map(photo => {
+//     return `<div class="photo-card">
+//   <img src="${photo.largeImageURL}" alt="${photo.tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes <span>${photo.likes}</span></b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views <span>${photo.views}</span></b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments <span>${photo.comments}</span></b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads <span>${photo.downloads}</span></b>
+//     </p>
+//   </div>
+// </div>`;
+//   });
+//   photoList.forEach(markupPhoto => {
+//     gallery.insertAdjacentHTML('beforeend', markupPhoto);
+//   });
+// }
+
+// //!
+// function onLoadMorePressed() {
+//   pages++;
+//   console.log(pages);
+//   onFormSubmit(event);
+// }
