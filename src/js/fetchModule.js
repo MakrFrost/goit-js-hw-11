48748222c8f914ea3d2c93d49aba9d83881bece0
MakrFrost <frostmark@ukr.net>
2022-09-30 23:03:05 +0300

    last update

--- src/js/fetchModule.js
+++ src/js/fetchModule.js
@@ -43,19 +43,3 @@ export default class PhotoApiService {
     this.needToFind = newQuery;
   }
 }
-
-//? код без класса(олд)
-// const API_WEB = 'https://pixabay.com/api/';
-// const API_KEY = '?key=30054612-8d338f00f07d687f9e2b96ac6&';
-
-// function fetchPhotos(needToFind, page) {
-//   return fetch(
-//     `${API_WEB}${API_KEY}q=${needToFind}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${page}`
-//   ).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-// export { fetchPhotos };
